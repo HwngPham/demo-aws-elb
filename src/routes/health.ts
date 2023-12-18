12302f5f281ac20373608c0971df5adce9129a44
HwngPham <quanghwng22@gmail.com>
2023-12-18 21:50:15 +0700

    Test CI

--- src/routes/health.ts
+++ src/routes/health.ts
@@ -1,6 +1,6 @@
 import { FastifyInstance } from "fastify";
 
 export const apiHealth = async (app: FastifyInstance) => {
-  app.get("/health", () => ({ message: "ok" }));
+  app.get("/health", () => ({ message: "ok 1" }));
   app.get("/", () => ({ message: "ok" }));
 };
