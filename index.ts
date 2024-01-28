7db8b8c4a8ba2743217022cbce9c6268d0277602
HwngPham <quanghwng22@gmail.com>
2024-01-28 21:38:29 +0700

    Config serverless

--- index.ts
+++ index.ts
@@ -1,13 +1,17 @@
 import "dotenv/config";
-
+import proxyLambda from "@fastify/aws-lambda";
 import { createServer } from "./src/main";
 
 const server = createServer();
 const port = process.env.PORT ? Number(process.env.PORT) : 8000;
 const host = process.env.HOST ?? "localhost";
+const isDev = process.env.NODE_ENV === "development";
 
-console.log(port, host);
-server.listen({ port, host }, () => {
-  console.log(`Server is running on http://${host}:${port}`);
-  console.log(server.printRoutes());
-});
+if (isDev) {
+  server.listen({ port, host }, () => {
+    console.log(`Development server is running on http://${host}:${port}`);
+    console.log(server.printRoutes());
+  });
+}
+
+export const handler = proxyLambda(server);
--- src/routes/health.ts
+++ src/routes/health.ts
@@ -2,5 +2,10 @@ import { FastifyInstance } from "fastify";
 
 export const apiHealth = async (app: FastifyInstance) => {
   app.get("/health", () => ({ message: "ok 1" }));
-  app.get("/", () => ({ message: "ok" }));
+  app.get("/", (req, _res) => ({
+    // @ts-expect-error un-typed inject
+    event: req?.awsLambda?.event,
+    // @ts-expect-error un-typed inject
+    context: req?.awsLambda?.context,
+  }));
 };
